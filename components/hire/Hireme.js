61b531009c89575d3468f682ce668e44c04048af
ujjawal <ujjawalrachhoya100@gmail.com>
2022-08-03 16:51:38 +0530

    Resume Update

--- components/hire/Hireme.js
+++ components/hire/Hireme.js
@@ -3,12 +3,12 @@ import Card from './Card'
 
 function Hireme() {
     return <>
-            <div className="bg-[#fffffe] mb-72">
-                <p className=' -my-20 dark:text-black text-white' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium sit explicabo provident iure, soluta ratione sunt fugit, architecto odio placeat autem cumque error commodi! Iure eius exercitationem nam harum rerum?</p>
-                <Card/>
-            </div>
-        </>
-    
+        <div className="bg-[#fffffe] mb-72">
+            <p className=' -my-20 dark:text-black text-white' >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium sit explicabo provident iure, soluta ratione sunt fugit, architecto odio placeat autem cumque error commodi! Iure eius exercitationem nam harum rerum?</p>
+            <Card />
+        </div>
+    </>
+
 }
 
 export default Hireme
