4c76a7071ee6cc8232cdc79d6ec50e4bf1641905
ujjawal <ujjawalrachhoya100@gmail.com>
2022-10-29 18:28:23 +0530

    Contact form update

---
+++ pages/api/contact.js
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer";
+export default async (req, res) => {
+    // console.log(req.body)
+    const { name, email, phone, message } = req.body;
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        service: 'gmail',
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD
+        }
+    });
+
+    try {
+        await transporter.sendMail({
+            from: email,
+            to: "jobsujjawal100@gmail.com",
+            subject: `${name} Message You`,
+            text: message,
+            html: `<p>You have contact form submission from portfolio</p>
+        <p><strong>Email: </strong> ${email}</p>
+        <p><strong>Phone: </strong> ${phone}</p>
+        <p><strong>Message: </strong> ${message}</p>
+      `
+        });
+        // res.send({status:200, message:'Hello'})
+    } catch (error) {
+        console.log(error.message)
+        return res.send({ status: 500, error: error.message });
+    }
+    return res.send({ status: 200, error: "" });
+};
