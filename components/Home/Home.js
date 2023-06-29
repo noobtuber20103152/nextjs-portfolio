dbce7fda27c153c2fd2e6694dca3c02f3e7df162
ujjawal <noobtuber20103152>
2023-06-30 00:12:13 +0530

    resume

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Codeeditor from './Codeeditor'
 import { AiFillFilePdf } from "react-icons/ai"
 import { SiBlogger } from "react-icons/si"
+import { BsYoutube } from 'react-icons/bs'
 function Home() {
     return <>
         <div className="bg-gradient-to-b pt-20 md:pt-40 text-white from-[#2d2546] to-[#181c22] h-auto ">
@@ -14,10 +15,11 @@ function Home() {
                     <div className='md:w-12/12 w-12/12 flex flex-wrap justify-center'>
                         <a href="https://blogtuber.vercel.app/" target="_blank" rel="noreferrer" className="hover:shadow-lg hover:-translate-y-1 hover:shadow-cyan-500/50  bg-transparent mx-2 my-2  font-bold text-lg py-3 duration-500 hover:bg-[#2d2546]  bg-[#373b3f] text-white px-4 rounded">Explore <SiBlogger className='inline text-2xl' /></a>
                         <a href="/Ujjawal_Rachhoya_SDE.pdf" target="_blank" rel="noreferrer" className="hover:shadow-lg hover:-translate-y-1 hover:shadow-yellow-500/50 bg-transparent mx-2 my-2 text-lg font-semibold border hover:border-gray-900 border-gray-400 duration-500 py-3 px-4   rounded">Resume <AiFillFilePdf className='inline text-2xl' /> </a>
+                        <a href="https://youtube.com/@explorewithme100" target="_blank" rel="noreferrer" className="hover:shadow-lg hover:-translate-y-1 hover:shadow-red-500/50 bg-transparent mx-2 my-2 text-lg font-semibold border hover:border-gray-900 border-gray-400 duration-500 py-3 px-4   rounded">YouTube <BsYoutube className='inline text-2xl text-red-400' /> </a>
                     </div>
                 </div>
                 <div className='flex justify-center h-auto'>
-                    <iframe className='w-full md:w-6/12 md:h-auto h-[30vh] md:mx-0 mx-2' src="https://github.com/sponsors/noobtuber20103152/card" title="Sponsor noobtuber20103152" ></iframe>
+                    <iframe className='w-full md:w-6/12 md:h-auto h-[30vh] md:mx-0 mx-2 text-white' src="https://github.com/sponsors/noobtuber20103152/card" title="Sponsor noobtuber20103152" ></iframe>
                 </div>
             </div>
             <Codeeditor />
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,8 @@ import Skills from ".././components/Skills/Skills";
 import Head from "next/head";
 import CodeEditor from ".././components/editor/CodeEditor";
 import "animate.css";
-import Project from ".././components/projects/Project";
 import Hireme from ".././components/hire/Hireme";
-import QnA from ".././components/QnA/QnA";
 import Contact from ".././components/contact/Contact";
-import Timeline from "../components/post-timeline/timeline";
-import ProjectFeatures from "../components/projects/projectFeatures";
 import Script from "next/script";
 
 function index() {
@@ -39,22 +35,7 @@ function index() {
       <Home />
       <Skills />
       <CodeEditor />
-
       <Hireme />
-      {/* {project.map((e, index) => {
-        return (
-          <>
-            <ProjectFeatures
-              id={index + 1}
-              live={e.live}
-              title={e.title}
-              description={e.description}
-            />
-          </>
-        );
-      })} */}
-      {/* <Timeline/> */}
-      {/* <QnA /> */}
       <Contact />
     </>
   );
